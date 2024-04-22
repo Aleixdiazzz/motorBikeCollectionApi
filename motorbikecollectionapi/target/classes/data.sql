8a529bf8143c31384e3e110afb3bc4a52de90f3a
Aleix Díaz <aleixdiazzz@gmail.com>
2024-04-22 18:07:50 +0200

    docu: added documentation

---
+++ motorbikecollectionapi/target/classes/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO MOTORBIKE (id, brand, model, year, horse_power, cubic_centimeters, collection_id) VALUES (1, 'HONDA', 'VFR', 1992, 120, 750, 1);
+INSERT INTO MOTORBIKE (id, brand, model, year, horse_power, cubic_centimeters, collection_id) VALUES (2, 'HONDA', 'CRF', 2017, 35, 250, 1);
+INSERT INTO MOTORBIKE (id, brand, model, year, horse_power, cubic_centimeters, collection_id) VALUES (3, 'TRIUMPH', 'BONNEVILLE', 2001, 80, 800, 1);
+INSERT INTO MOTORBIKE (id, brand, model, year, horse_power, cubic_centimeters, collection_id) VALUES (4, 'SCOOTER', 'SCOOTER', 2015, 1, 125, 2);
+INSERT INTO MOTORBIKE (id, brand, model, year, horse_power, cubic_centimeters, collection_id) VALUES (5, 'No tiene', 'le dan miedo', 0, 0, 0, 3);
+
+
+INSERT INTO OWNER (id, name, surname, age, phone, email, collection_id) VALUES (1, 'Aleix', 'Diaz Llabres', 22, 648007507, 'aleixdiaz@kumointernet.com', 1);
+INSERT INTO OWNER (id, name, surname, age, phone, email, collection_id) VALUES (2, 'Dario Alvaro', 'Reig Buades', 22, 666420420, 'dreigbuades@cifpfbmoll.com', 2);
+INSERT INTO OWNER (id, name, surname, age, phone, email, collection_id) VALUES (3, 'David', 'Lozano Cabeza', 13, 666696969, 'dlozano@cifpfbmoll.com', 3);
+
+/*- Robert
+- Rafa
+- Oscar
+- Angels
+- Santi
+- Dani
+- Marc
+- ??
+
+- Maximo*/
