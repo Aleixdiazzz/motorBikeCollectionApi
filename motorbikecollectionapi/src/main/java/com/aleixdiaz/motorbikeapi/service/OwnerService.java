689f236a13bc5595057c88c2fde3e6bad08efbac
Aleix Díaz <aleixdiazzz@gmail.com>
2024-04-24 16:28:45 +0200

    docu: added documentation

--- motorbikecollectionapi/src/main/java/com/aleixdiaz/motorbikeapi/service/OwnerService.java
+++ motorbikecollectionapi/src/main/java/com/aleixdiaz/motorbikeapi/service/OwnerService.java
@@ -65,6 +65,7 @@ public class OwnerService {
                 }
                 if (motorbike.getCollectionId() == ownerTwoCollectionid) {
                     ownerTwoCollectionSize++;
+                    // comment
                 }
             }
 
